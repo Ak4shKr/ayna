0746210481cf8d42ff42fa53de4abad7a1749261
Ak4shKr <akashkmr7667@gmail.com>
2025-02-13 22:23:40 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-semibold underline text-black text-lg">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default App;
